a850c7e937ee6a78e058b27c6febe6fd3013e933
Ramzan Sheikh <s.ramzan@protonmail.com>
2021-07-29 10:17:38 -0400

    Hourly: add precipitation icons

--- app/src/main/java/ca/ramzan/atmostate/ui/Hourly.kt
+++ app/src/main/java/ca/ramzan/atmostate/ui/Hourly.kt
@@ -1,26 +1,20 @@
 package ca.ramzan.atmostate.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ca.ramzan.atmostate.network.Hourly
-import ca.ramzan.atmostate.network.Weather
-import ca.ramzan.atmostate.ui.theme.Lime200
 import coil.compose.rememberImagePainter
-import java.util.*
+import com.ramzan.atmostate.R
 import kotlin.math.roundToInt
 
 @Composable
@@ -35,30 +29,80 @@ fun HourlyForecast(listState: LazyListState, hourly: List<Hourly>) {
             hourly.forEach { hour ->
                 hour.run {
                     Row(
-                        modifier = Modifier.fillMaxWidth().clickable {  }.padding(16.dp, 8.dp, 16.dp, 12.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { }
+                            .padding(16.dp, 8.dp, 16.dp, 12.dp),
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Text(TimeFormatter.toHourOfDay(dt))
                         Column {
-                           Row(verticalAlignment = Alignment.Bottom) {
-                               Image(
-                                   painter = rememberImagePainter("https://openweathermap.org/img/wn/${weather.first().icon}@4x.png"),
-                                   contentDescription = "Forecast image",
-                                   modifier = Modifier.size(48.dp)
-                               )
-                               Text("${temp.roundToInt()}°", fontSize= 32.sp)
-                           }
+                            Row(verticalAlignment = Alignment.Bottom) {
+                                Image(
+                                    painter = rememberImagePainter("https://openweathermap.org/img/wn/${weather.first().icon}@4x.png"),
+                                    contentDescription = "Forecast image",
+                                    modifier = Modifier.size(48.dp)
+                                )
+                                Text("${temp.roundToInt()}°", fontSize = 32.sp)
+                            }
                             Text("Feels like ${feelsLike.roundToInt()}")
                         }
 //                            Text(weather.description.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
 //                        Wind(windSpeed, windGust, degreeToDirection(windDeg))
-                        Text("${pop.roundToInt()}%", Modifier.wrapContentWidth(Alignment.End))
-//                        rain?.let { Rain(it.hour) }
-//                        snow?.let { Snow(it.hour) }
+                        Column(
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.rain_cloud),
+                                    contentDescription = "Probability of precipitation",
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(
+                                    "${pop.roundToInt()}%",
+                                    Modifier.wrapContentWidth(Alignment.End)
+                                )
+                            }
+                            rain?.let { HourRain(it.hour) }
+                            snow?.let { HourSnow(it.hour) }
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun HourRain(rain: Double) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(R.drawable.rain_drop),
+            contentDescription = "Rain",
+            modifier = Modifier.padding(end = 8.dp)
+        )
+        Text(text = "${"%.1f".format(rain)}mm")
+    }
+}
+
+@Composable
+fun HourSnow(snow: Double) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(R.drawable.snow_flake),
+            contentDescription = "Rain",
+            modifier = Modifier.padding(end = 8.dp)
+        )
+        Text(text = "${"%.1f".format(snow)}mm")
     }
 }
